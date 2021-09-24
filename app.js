0e4f502958b8c4949904548fbb680fe970155cf7
Mary V <marycatherine.villondo@cit.edu>
2021-09-24 17:11:45 +0800

    Updated Documentation

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
     Description: This project is a requirement for submission in IT461 - Advance Web Systems.The languages used here are html, css and vanila js. Patuti is 2D Game where in the controlled character (Patuti) is a space alien which aim is to stay alive. The user controlls the character to ensure that it must evade the collision with the bullets.  
 */
 // Canvas setup
+// Global Variables
 const canvas = document.getElementById('canvas1');
 const context = canvas.getContext('2d');
 canvas.width = 880;
@@ -13,7 +14,7 @@ let gameFrame = 1;
 let gameSpeed = 1;
 let gameOver = false;
 
-context.font = '3.125rem Poppins';
+context.font = '2.5rem Poppins';
 
 //Mouse Interactivity 
 let canvasPosition = canvas.getBoundingClientRect();
@@ -23,6 +24,7 @@ const mouse = {
     click:false
 }
 
+//Events everytime a mouse is clicked or not
 canvas.addEventListener('mousedown', function(event){
     mouse.click = true;
     mouse.x = event.x - canvasPosition.left;
@@ -35,6 +37,7 @@ canvas.addEventListener('mouseup', function(event){
 });
 
 //Player  
+//Sprite images for moving left and right
 const playerLeft = new Image();
 playerLeft.src = './assets/images/sprites/patuti_left.png';
 const playerRight = new Image();
@@ -43,7 +46,7 @@ playerRight.src = './assets/images/sprites/patuti_right.png';
 class Player {
     constructor(){
         this.x = canvas.width/2;
-        this.y = canvas.height / 2;
+        this.y = (canvas.height/2);
         this.radius = 50;
         this.frameX = 4;
         this.frameY = 0;
@@ -53,12 +56,10 @@ class Player {
     }
 
     update(){
+        // Distance calculation between patuti and mouse cursor position
         const distance_x = this.x - mouse.x;
         const distance_y = this.y - mouse.y;
-        // Angle calculations
-        /* let theta = Math.atan2(distance_x, distance_y);
-        this.angle = theta; */
-        //Player Movement Speed 
+        //Player Movement Speed Calculation
         if(mouse.x != this.x){
             this.x -= distance_x/30;
         }
@@ -76,7 +77,7 @@ class Player {
             context.lineTo(mouse.x, mouse.y);
             context.stroke();
         }
-        // Skeleton for Patuti
+        // Uncomment to view geometic skeleton for Patuti
         /* context.fillStyle ='red';
         context.beginPath();
         context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
@@ -84,6 +85,7 @@ class Player {
         context.closePath();
         context.fillRect(this.x, this.y, this.radius, 10); */
 
+        //Creating the sprites in the canvas to prepare for animation
         context.save();
         context.translate(this.x, this.y);
         if(this.x >= mouse.x){
@@ -96,9 +98,43 @@ class Player {
 }
 const player = new Player();
 
+class HealthBar{
+    constructor(x, y, width, height, maxHealth, color){
+        this.x = x;
+        this.y  = y;
+        this.width = width;
+        this.height = height;
+        this.maxHealth = maxHealth;
+        this.maxWidth = width;
+        this.health = maxHealth;
+        this.color = color;
+    }
+
+    show(){
+        context.lineWidth = 5;
+        context.strokeStyle = "#202830";
+        context.fillStyle = this.color;
+        context.fillRect(this.x, this.y, this.width, this.height);
+        context.strokeRect(this.x, this.y, this.maxWidth, this.height);
+    }
+
+    updateHealth(value){
+        this.health = value;
+        this.width = (this.health/this.maxHealth) *this.maxWidth;
+    }
+}
+
+const healthBarWidth = 200;
+const healthBarHeight = 30;
+const positionXHealthBar = canvas.width - (healthBarWidth + 15);
+const positionYHealthBar = canvas.height - (healthBarHeight + 20);
+const healthBar = new HealthBar (positionXHealthBar,  positionYHealthBar, healthBarWidth, healthBarHeight, health, "green");
+
 //Bullets  
 const bulletsVerticalArray = [];
 const bulletsHorizontalArray = [];
+
+//Sprite Images for the bullets
 const bulletImage_Vertical = new Image();
 bulletImage_Vertical.src = 'assets/images/bullet_v.png'
 const bulletImage_Horizontal = new Image();
@@ -112,17 +148,14 @@ class BulletHorizontal {
         this.y = Math.random() * (canvas.height - 150) + 90;
         this.radius = 50;
         this.speed = Math.random() * 1 + 1;
-        this.distance
-        this.frame = 0 ;
-        this.frameX  = 0;
-        this.frameY = 0;
+        this.distance;
         this.sound = Math.random() <= 0.5 ? 'sound1' : 'sound2';
         this.spriteWidth = 100;
         this.spriteHeight = 50;
     }
 
     draw(){
-        //Skeleton bullet
+        //Uncomment to view geometric skeleton for horizontal bullet
         /* context.fillStyle = 'purple';
         context.beginPath();
         context.arc(this.x,this.y, this.radius, 0, Math.PI * 2);
@@ -133,14 +166,12 @@ class BulletHorizontal {
     }
 
     update(){
+        //Bullet path
         this.x -= this.speed;
+        // Caculation for collision using the pythagorian theorem.
         const distance_x = this.x - player.x;
         const distance_y = this.y - player.y;
         this.distance = Math.sqrt(distance_x * distance_x + distance_y * distance_y);
-        //Bullet recursion on random horizontal point
-        /* if(distance < this.radius + player.radius){
-            handleGameOver();
-        } */
     }
 
 }
@@ -149,7 +180,7 @@ const bulletHorizontal = new BulletHorizontal();
 //Vertical Bullets
 class BulletVertical{
     constructor(){
-        this.x = Math.random() * (canvas.width);
+        this.x = Math.random() * (canvas.width - 150) + 90;
         this.y = -100;
         this.radius = 50;
         this.speed = Math.random() * 1 + 1;
@@ -161,17 +192,17 @@ class BulletVertical{
     }
 
     update(){
+        //Bullet path
         this.y += this.speed;
+        // Caculation for collision using the pythagorian theorem.
         const distance_x = this.x - player.x;
         const distance_y = this.y - player.y;
-        this.speed = Math.random() * 1 + 1;
-        // Collision detection
-        this.distance = Math.sqrt(distance_x * distance_x + distance_y * distance_y); //pythagorean theorem
+        this.distance = Math.sqrt(distance_x * distance_x + distance_y * distance_y); 
         
     }
 
     draw(){
-        //Skeleton bullet
+        //Uncommetn to view geometric skeleton bullet
         /* context.fillStyle = 'blue';
         context.beginPath();
         context.arc(this.x,this.y, this.radius, 0, Math.PI * 2);
@@ -182,16 +213,20 @@ class BulletVertical{
     }
 }
 
+//Audio file sources for bullet collision
 const audio_Bullets1 = document.createElement('audio');
 audio_Bullets1.src = './assets/audio/vgmenuhighlight.ogg';
 const audio_Bullets2 = document.createElement('audio');
 audio_Bullets2.src = './assets/audio/vgmenuselect.ogg';
 
-function handleBulletsVertical(){
+//Function for handling bullets
+function handleBullets(){
     if(!gameOver){
         if(gameFrame % 225 == 0){
             bulletsVerticalArray.push(new BulletVertical());
+            bulletsHorizontalArray.push(new BulletHorizontal());
             //(console.log(bulletsVerticalArray.length));
+            //(console.log(bulletsHorizontalArray.length));
         }
         for(let i = 0; i < bulletsVerticalArray.length; i++){
             bulletsVerticalArray[i].draw();
@@ -221,21 +256,14 @@ function handleBulletsVertical(){
                     }
                     i--; 
                     health = health-20;
+                    
                     handleGameOver();
                 }
-    
+                healthBar.updateHealth(health);
             }
     
         }
-    }
-}
 
-function handleBulletsHorizontal(){
-    if(!gameOver){
-        if(gameFrame % 225 == 0){
-            bulletsHorizontalArray.push(new BulletHorizontal());
-            //(console.log(bulletsHorizontalArray.length));
-        }
         for(let i = 0; i < bulletsHorizontalArray.length; i++){
             bulletsHorizontalArray[i].draw();
             bulletsHorizontalArray[i].update();
@@ -263,9 +291,10 @@ function handleBulletsHorizontal(){
                     }
                     i--; 
                     health = health-20;
+                    
                     handleGameOver();
                 }
-               
+                healthBar.updateHealth(health);
             }
     
         }
@@ -276,6 +305,7 @@ function handleBulletsHorizontal(){
 const background = new Image();
 background.src = 'assets/images/area.png';
 
+//Drawing the platform in the canvas element
 function handleBackground(){
     var wrh = background.width / background.height;
     var newWidth = canvas.width;
@@ -287,28 +317,27 @@ function handleBackground(){
     var xOffset = newWidth < canvas.width ? ((canvas.width - newWidth) / 3) : 0;
     var yOffset = newHeight < canvas.height ? ((canvas.height - newHeight) / 3) : 0;
 
-    context.drawImage(background, xOffset, yOffset + 350, newWidth, newHeight/3);
+    context.drawImage(background, xOffset, yOffset + 300, newWidth, newHeight/3);
 }
 
+//Game-over
 const audio_gameOver = document.createElement('audio');
 audio_gameOver.src = './assets/audio/gameover.ogg';
 
-//Game-over
 function handleGameOver() {
     context.fillStyle = 'red';
-    context.fillText('GAME OVER', (canvas.width/2) - 100 , (canvas.height/2) - 50);
+    context.fillText('GAME OVER', (canvas.width/2) - 120 , (canvas.height/2) - 50);
     gameOver = true;
     audio_gameOver.play();
 
 }
 
-
 //Animation Loop
 function animate(){
     context.clearRect(0,0,canvas.width,canvas.height);
     handleBackground();
-    handleBulletsVertical();
-    handleBulletsHorizontal();
+    handleBullets();
+    healthBar.show();
     player.update();
     player.draw();
     context.fillStyle = '#202830';
@@ -319,6 +348,7 @@ function animate(){
 }
 animate();
 
+// Function to ensure that points stay the same event if window is resized
 window.addEventListener('resize', function(){
     canvasPosition = canvas.getBoundingClientRect();
 });
